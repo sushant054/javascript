a2aca258f13fdfd81b14c81ef8a011d498c13e15
Sushant Nalage <145271874+sushant054@users.noreply.github.com>
2024-01-26 13:45:25 +0530

    multiplication table using prompt

---
+++ multiplication table/promptmul.js
@@ -0,0 +1,7 @@
+let n= prompt("Enter the no which you want to print table");
+n=parseInt(n);
+//if we want to run this code link this file to .html file then run .html file.....
+for(i=n;i<=n*10;i=i+n)
+{
+    console.log(i);
+}
