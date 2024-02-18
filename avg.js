7fef5bcbcf6b38485b2ded1f172ef3942b054577
sushant054 <sushantnalage54@gmail.com>
2024-02-19 01:16:05 +0530

    cal.avg and printing table using function...

---
+++ avg.js
@@ -0,0 +1,14 @@
+function avg(a,b,c){
+    avg=(a+b+c)/3;
+    console.log(avg);
+}
+avg(2,5,7);
+// printing table using function
+function printtable(n)
+{
+    for(let i=n;i<=n*10;i+=n)
+    {
+        console.log(i);
+    }
+}
+printtable(10);
