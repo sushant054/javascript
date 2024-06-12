9d49e36a467678ce55e2ce6ea5c18fb9d580ebab
sushant054 <sushantnalage54@gmail.com>
2024-06-12 19:33:30 +0530

    find larger no. in array..

--- jshosting.js
+++ jshosting.js
@@ -6,4 +6,5 @@ x = 5; // Assign 5 to x
 elem = document.getElementById("demo"); // Find an element 
 elem.innerHTML = x;           // Display x in the element
 
-var x; // Declare x
+var x; // Declare x   
+
