2c84b4e462953aae81173a80cc44b232f7fdd385
sushant054 <sushantnalage54@gmail.com>
2024-06-13 22:11:50 +0530

    removes extra char..

---
+++ extrachar.js
@@ -0,0 +1,16 @@
+//Write a JavaScript function to extra unique characters from a string
+//Example:str=“abcdabcdefgggh”
+//ans=“abcdefgh”
+let str="abcdabcdefgggh";
+function getUnique (str){
+    let ans="";
+    for(let i=0; i< str.length;i++)
+        {
+            let currChar=str[i];
+            if(ans.indexOf(currChar)==-1){
+                ans+=currChar;
+            }
+        }
+        return ans;
+}
+getUnique(str);
