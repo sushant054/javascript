9e2d495e3705f826435c777647dd5a2acb8b7dc3
sushant054 <sushantnalage54@gmail.com>
2024-06-13 23:18:46 +0530

    random number generator in between range....

--- randomnum.js
+++ randomnum.js
@@ -1,2 +1,10 @@
-Math.floor(math.random()*10)+1;
+//Math.floor(math.random()*10)+1;
 //this line will give random number between 1 to 10 in consol...(firstly create .html file for this...)
+let start=100;
+let end=300;
+function randomNmumberGenerator (start,end){
+    let diff =end -start ;
+    return Math.floor(Math.random()*diff)+start;
+}
+let ans=randomNmumberGenerator(start,end);
+console.log(ans);
